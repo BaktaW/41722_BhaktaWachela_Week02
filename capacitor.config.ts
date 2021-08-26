a7f6e8d06a6ae0e20d27b86a084201fd78a45ac5
BaktaW <89568507+BaktaW@users.noreply.github.com>
2021-08-26 13:07:03 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '41722-BhaktaWachela',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
